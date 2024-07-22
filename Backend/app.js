42bbaab960f911bfe2a4129e5aceece5a07c9af5
johaen <jgnanak1@my.centennialcollege.ca>
2024-07-22 19:00:03 -0400

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
-
+const cors = require('cors');
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 app.use("/books", router); // localhost:5000/books
 
 mongoose
@@ -14,6 +15,6 @@ mongoose
   )
   .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(3000);
+    app.listen(5000);
   })
   .catch((err) => console.log(err));
--- client/src/components/Book/Books.js
+++ client/src/components/Book/Books.js
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from "react";
-
-import axios from 'axios'
-const URL = "http://localhost:3000/books";
-
-const fetchHandler = async() => {
-           return await axios.get(URL).then((res)=> res.data)
-}
+import "./Book.css";
+import axios from "axios";
+import Book from "./Book";
+const URL = "http://localhost:5000/books";
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
 const Books = () => {
-    const [books, setBooks] = useState();
-    useEffect(() => {
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setBooks(data.books));
+  }, []);
 
-     
-    fetchHandler().then((data) =>setBooks(data));
-
-    });
-    console.log(books);
+  console.log(books);
   return (
-    <div>All Books are here</div>
-  )
-}
+    <div>
+      <div>
+        <img src="">
+        </img></div>
+      <ul>
+        {books &&
+          books.map((book, i) => (
+            <li key={i}>
+              <Book book={book} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Books
+export default Books;

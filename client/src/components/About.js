c91d1784690a93c3a07b85441be3bcbb9f074631
johaen <jgnanak1@my.centennialcollege.ca>
2024-07-23 15:30:46 -0400

    update button and book detail

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 const About = () => {
   return (
-    <div>This is the About page</div>
-  )
-}
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h2">
+          This is a CRUD Application
+        </Typography>
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h3">
+          By MERN STACK
+        </Typography>
+      </Box>
+    </div>
+  );
+};
 
-export default About
+export default About;
